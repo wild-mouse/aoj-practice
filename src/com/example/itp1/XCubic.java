039e316495ad87421f55caaeb0641db6804db813
Tsukasa Noguchi <t-noguchi@skynext.co.jp>
2018-12-24 20:13:17 +0900

    Add solution of X Cubic

---
+++ src/com/example/itp1/XCubic.java
@@ -0,0 +1,16 @@
+package com.example.itp1;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class XCubic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int x = Integer.parseInt(sc.nextLine());
+            System.out.println(x * x * x);
+        } catch (NoSuchElementException | NumberFormatException e) {
+            System.out.println(e);
+        }
+    }
+}
