cbefc912efcedcfda823927c11f53230bba7c838
Tsukasa Noguchi <wildmouse0813@gmail.com>
2018-12-24 20:39:46 +0900

    Add solution of Watch

---
+++ src/com/example/itp1/Watch.java
@@ -0,0 +1,24 @@
+package com.example.itp1;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Watch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int s = Integer.parseInt(sc.nextLine());
+            if (s >= 86400 || s < 0) {
+                System.out.println("Input is incorrect");
+                return;
+            }
+            int h = s / (60 * 60);
+            int r = s % (60 * 60);
+            int m = r / 60;
+            r = s % 60;
+            System.out.printf("%d:%d:%d\n", h, m, r);
+        } catch (NoSuchElementException | NumberFormatException e) {
+            System.out.println(e);
+        }
+    }
+}
