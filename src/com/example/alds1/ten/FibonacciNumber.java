7e93a82bef3ea8f6c250b2f0c4ad4fc56bb2b9b3
Tsukasa Noguchi <wildmouse0813@gmail.com>
2018-12-30 07:47:38 +0900

    Update solution of Fibonacci Number(solved)

--- src/com/example/alds1/ten/FibonacciNumber.java
+++ src/com/example/alds1/ten/FibonacciNumber.java
@@ -8,21 +8,27 @@ public class FibonacciNumber {
         Scanner sc = new Scanner(System.in);
         try {
             int n = Integer.parseInt(sc.nextLine());
-            if (n < 0 || n > 45) {
+            int MIN = 0, MAX = 44;
+            if (n < MIN || n > MAX) {
                 System.out.println("Condition is invalid.");
                 return;
             }
-            System.out.println(fibonacci(n));
+            int[] fs = new int[MAX + 1];
+            System.out.println(fibonacci(n, fs));
         } catch (NoSuchElementException
                 | NumberFormatException e) {
             System.out.println(e);
         }
     }
 
-    private static int fibonacci(int n) {
+    private static int fibonacci(int n, int[] fs) {
         if (n == 0 || n == 1) {
             return 1;
         }
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        if (fs[n] != 0) {
+            return fs[n];
+        }
+        fs[n] = fibonacci(n - 1, fs) + fibonacci(n - 2, fs);
+        return fs[n];
     }
 }
