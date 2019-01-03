7481da8d00d2e45f8a81847c6e5e6ebb1e1aecf4
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-01-04 06:53:40 +0900

    Update solution of Print Many Hello World

--- src/com/example/itp1/three/PrintManyHelloWorld.java
+++ src/com/example/itp1/three/PrintManyHelloWorld.java
@@ -5,8 +5,6 @@ import java.util.stream.IntStream;
 public class PrintManyHelloWorld {
     public static void main(String[] args) {
         IntStream.rangeClosed(1, 1000)
-                .forEach(i -> {
-                    System.out.println("Hello World");
-                });
+                .forEach(i -> System.out.println("Hello World"));
     }
 }
